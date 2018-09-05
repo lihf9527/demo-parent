3dd9c268b9a97f6e1d515b3b688dc6c7f812f8c7
haifeng.li <haifeng.li@sao.so>
2018-09-05 19:04:47 +0800

    添加子项目

---
+++ demo-querydsl-mongo/src/main/java/com/example/demoquerydslmongo/DemoQuerydslMongoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoquerydslmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQuerydslMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQuerydslMongoApplication.class, args);
+    }
+}
