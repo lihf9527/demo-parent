30d33da4eddc855b191e0b3de2dae726b326b51c
lihf <lihf@sina.cn>
2018-11-19 02:32:23 +0800

    添加 NumberUtils 工具类

---
+++ demo/src/main/java/com/example/demo/util/NumberUtils.java
@@ -0,0 +1,16 @@
+package com.example.demo.util;
+
+import java.text.DecimalFormat;
+
+public class NumberUtils {
+    private NumberUtils() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new DecimalFormat("#.##").format(34234321.43214));
+        System.out.println(new DecimalFormat("00.00").format(1.43214));
+        System.out.println(new DecimalFormat("##.00").format(1.43214));
+        System.out.println(new DecimalFormat("#.##%").format(0.54576));
+        System.out.println(new DecimalFormat("#.00%").format(0.54576));
+    }
+}
