209041114fe109a7d391ea784349e4ec6678241f
haifeng <lihf@sina.cn>
2018-11-21 14:17:17 +0800

    升级 spring boot 版本

--- demo-mybatis/src/main/java/com/example/MybatisApplication.java
+++ demo-mybatis/src/main/java/com/example/MybatisApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoMybatisApplication {
+public class MybatisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoMybatisApplication.class, args);
+        SpringApplication.run(MybatisApplication.class, args);
     }
 }
