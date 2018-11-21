c45022486f737b9e62fc0c4b02a18d694fa023e9
haifeng <lihf@sina.cn>
2018-11-21 11:02:25 +0800

    删除util子项目

--- demo-querydsl-jpa/src/main/java/com/example/factory/BookFacotry.java
+++ demo-querydsl-jpa/src/main/java/com/example/factory/BookFacotry.java
@@ -1,14 +1,16 @@
 package com.example.factory;
 
 import com.example.model.Book;
-import com.example.util.RandomUtils;
+
+import java.util.Random;
 
 public class BookFacotry {
     private static String[] names = {"西游记", "水浒传", "三国演义", "红楼梦"};
     private static String[] authors = {"吴承恩", "施耐庵", "罗贯中", "曹雪芹"};
 
     public static Book generateBook() {
-        int i = RandomUtils.nextInt(names.length);
+        Random random = new Random();
+        int i = random.nextInt(names.length);
         Book book = new Book();
         book.setName(names[i]);
         book.setAuthor(authors[i]);
