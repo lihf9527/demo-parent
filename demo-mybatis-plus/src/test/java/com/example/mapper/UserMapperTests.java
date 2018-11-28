884303c82892d456baccfe1425c819c8960a6785
haifeng <lihf@sina.cn>
2018-11-28 19:15:53 +0800

    添加mybatis-plus单元测试

--- demo-mybatis-plus/src/test/java/com/example/mapper/UserMapperTests.java
+++ demo-mybatis-plus/src/test/java/com/example/mapper/UserMapperTests.java
@@ -1,5 +1,8 @@
 package com.example.mapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,32 @@ public class UserMapperTests {
     private UserMapper userMapper;
 
     @Test
-    public void selectTest() {
+    public void insertTest() {
+        User user = new User();
+        user.setId(6L);
+        user.setName("Tom");
+        user.setAge(18);
+        user.setEmail("tom@sina.com");
+        userMapper.insert(user);
+    }
+
+    @Test
+    public void selectByIdTest() {
+        System.out.println(userMapper.selectById(6));
+    }
+
+    @Test
+    public void selectAllTest() {
         userMapper.selectList(null).forEach(System.out::println);
+    }
+
+    @Test
+    public void selectPageTest() {
+        IPage<User> page = userMapper.selectPage(new Page<>(2, 3), null);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getPages());
+        page.getRecords().forEach(System.out::println);
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
     }
 }
