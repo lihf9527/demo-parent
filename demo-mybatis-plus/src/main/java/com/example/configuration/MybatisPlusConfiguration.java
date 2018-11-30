f5d83db74359978b4a17cee47bfb633e55b42b0b
haifeng <lihf@sina.cn>
2018-11-30 19:13:24 +0800

    优化

--- demo-mybatis-plus/src/main/java/com/example/configuration/MybatisPlusConfiguration.java
+++ demo-mybatis-plus/src/main/java/com/example/configuration/MybatisPlusConfiguration.java
@@ -11,4 +11,17 @@ public class MybatisPlusConfiguration {
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
+
+    public static void main(String[] args) {
+        System.out.println(calc(20));
+        System.out.println(calc(0));
+    }
+
+    public static int calc(int month) {
+        if (month < 1)
+            return -1;// 月份小于0,返回-1
+        if (month == 1 || month == 2)
+            return 1;// 第一月和第二月都是一对兔子
+        return calc(month - 1) + calc(month - 2);// 递归计算兔子
+    }
 }
