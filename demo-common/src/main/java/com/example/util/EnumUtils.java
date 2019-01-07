5236db4274aecbc1822e38f41cee980242f28ef7
haifeng <lihf@sina.cn>
2019-01-07 13:32:24 +0800

    EnumUtils中新增方法

--- demo-common/src/main/java/com/example/util/EnumUtils.java
+++ demo-common/src/main/java/com/example/util/EnumUtils.java
@@ -13,24 +13,33 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
 public class EnumUtils {
-    private static final ConcurrentMap<String, Map<?, EnumMsg>> enumMap = new ConcurrentHashMap<>();
+    private static final ConcurrentMap<String, Map<?, EnumMsg>> ENUM_MAP = new ConcurrentHashMap<>();
 
     private EnumUtils() {
     }
 
-    public static <E extends EnumMsg> List<EnumMsg> getEnums(Class<E> enumClass) {
+    public static <E extends EnumMsg> List<E> getEnums(Class<E> enumClass) {
+        return getEnumMsgMap(enumClass).values().stream().map(enumClass::cast).collect(Collectors.toList());
+    }
+
+    public static <E extends EnumMsg> List<EnumMsg> getEnumMsgs(Class<E> enumClass) {
         return new ArrayList<>(getEnumMsgMap(enumClass).values());
     }
 
     public static <E extends EnumMsg<V>, V> String getEnumName(V value, Class<E> enumClass) {
-        return toEnum(value, enumClass).getName();
+        return toEnumMsg(value, enumClass).getName();
     }
 
     public static <E extends EnumMsg<V>, V> boolean isValid(V value, Class<E> enumClass) {
-        return !toEnum(value, enumClass).isEmpty();
+        return !toEnumMsg(value, enumClass).isEmpty();
     }
 
-    public static <E extends EnumMsg<V>, V> EnumMsg toEnum(V value, Class<E> enumClass) {
+    public static <E extends EnumMsg<V>, V> E toEnum(V value, Class<E> enumClass) {
+        EnumMsg enumMsg = toEnumMsg(value, enumClass);
+        return enumClass.isInstance(enumMsg) ? enumClass.cast(enumMsg) : null;
+    }
+
+    public static <E extends EnumMsg<V>, V> EnumMsg toEnumMsg(V value, Class<E> enumClass) {
         if (value == null || enumClass == null)
             return EmptyEnum.NULL;
 
@@ -40,7 +49,7 @@ public class EnumUtils {
 
     private static <E extends EnumMsg> Map<?, EnumMsg> getEnumMsgMap(Class<E> enumClass) {
         String className = enumClass.getName();
-        Map<?, EnumMsg> enumMsgMap = enumMap.get(className);
+        Map<?, EnumMsg> enumMsgMap = ENUM_MAP.get(className);
         if (enumMsgMap != null)
             return enumMsgMap;
 
@@ -52,7 +61,7 @@ public class EnumUtils {
             enumMsgs = new EnumMsg[0];
         }
         enumMsgMap = Arrays.stream(enumMsgs).collect(Collectors.toMap(EnumMsg::getValue, enumMsg -> enumMsg));
-        enumMap.putIfAbsent(className, enumMsgMap);
+        ENUM_MAP.putIfAbsent(className, enumMsgMap);
         return enumMsgMap;
     }
 }
